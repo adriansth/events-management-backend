dfdd5e981cc389bd116ac1cce0bf6dcd7943a97c
adriansth <soto.herrera.adrian@gmail.com>
2024-10-14 14:21:42 -0600

    feat: implement event crud routes

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI, Depends
 import firebase_admin
 from firebase_admin import credentials, firestore
 from handlers.user_handler import *
+from handlers.event_handler import *
 from models.user import User
+from models.event import Event
 from middleware.auth import verify_token
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -21,20 +23,47 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# create user
 @app.post("/user/")
 def create_user_route(user: User):
     return create_user(db, user)
 
+# get user by uid
 @app.get("/user/{uid}")
 def get_user_by_uid_route(uid: str):
     return get_user_by_uid(db, uid)
 
-@app.put("/users/{uid}")
+# update user by uid
+@app.put("/user/{uid}")
 def update_user_by_uid_route(uid: str, user_update: dict):
     return update_user_by_uid(db, uid, user_update)
 
-@app.delete("/users/{uid}")
+# delete user by uid
+@app.delete("/user/{uid}")
 def delete_user_by_uid_route(uid: str):
     return delete_user_by_uid(db, uid)
 
+# create event
+@app.post("/event/")
+def create_event(event: Event):
+   return create_event(db, event)
 
+# get events by organizer
+@app.get("/event/{organizer}")
+def get_events_by_organizer(organizer: str):
+    return get_events_by_organizer(db, organizer)
+
+# get event by id
+@app.get("/event/{event_id}")
+def get_event_by_id(event_id: str):
+    return get_event_by_id(db, event_id)
+
+# update event by id
+@app.put("/event/{event_id}")
+def update_event_by_id(event_id: str, event_update: dict):
+    return update_event_by_id(db, event_id, event_update)
+
+# delete event by id
+@app.delete("/event/{id}")
+def delete_event_by_id(event_id: str):
+    return delete_event_by_id(db, event_id)
